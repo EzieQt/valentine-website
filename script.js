89fd3b6c5268ff45ef61791a9e284393a8afbefd
EzieQt <143060423+EzieQt@users.noreply.github.com>
2025-02-12 16:30:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ noButton.addEventListener('click', () => {
 
   // Change the image to something funny
   if (noCount % 2 === 0) {
-    valentineImage.src = 'funny-image1.jpg'; // Add a funny image
+    valentineImage.src = 'min/funny-image1.jpg'; // Add a funny image
   } else {
     valentineImage.src = 'C:/Users/ACER/Downloads/ValentinesWeb/ValenFoto/Bred.png'; // Add another funny image
   }
